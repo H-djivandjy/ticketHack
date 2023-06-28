c6e82e87f53430b5a30132f4cedd09193bd4b560
H-Djivandjy <houned.djivandjy@eemi.com>
2023-06-28 10:05:09 +0200

    updating backend trip search according to user input

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,44 +1,101 @@
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
+const moment = require("moment")
 
 //----------- Model Import -----------
 const Trip = require('../models/trips');
 
 
 //----------- Module Function Import -----------
-const {checkField} = require('../modules/checkField')
+const {checkField} = require('../modules/checkField');
+const { format } = require('morgan');
 
 
-router.get('/', (req,res)=>{
-  let userInput = []
-  const departureInput = req.body.departure
-  const arrivalInput = req.body.arrival
-  userInput.push(departureInput,arrivalInput )
-  // console.log(userInput)
+//!________________________________________________________________ Methode 1
+// router.get('/', (req,res)=>{
+//   //* ------------- FRONT INPUT EXTRACTION -------------
+//   const departureInput = req.body.departure
+//   const arrivalInput = req.body.arrival
+//   const dateInput = req.body.date
 
-  if (!departureInput || !arrivalInput){
-    res.json({ result: false, error: 'Empty field'})
-  }else{
-    res.json({ result: true, message: userInput})
+//   //* ------------- EMPTY INPUT CHECKING -------------
+//   if(!checkField(req.body, ["departure", "arrival", "date"])){
+//     res.json({result:   false, error: 'Missing or empty fields'})
+//     return;
+//   } 
+  
+//   //* ---------- FrontEnd DATE FORMATING -----------------
+//   const dateYear = dateInput.substr(6, 9);
+//   const dateMonth = dateInput.substr(3, 2);
+//   const dateDay = dateInput.substr(0, 2);
 
+//   const frontDateFormatted = `${dateYear}-${dateMonth}-${dateDay}`
+
+//   //__________________________________________
+//   //// const regexPattern = new RegExp(`^${frontDateFormatted}`);
+//   //// date: { $regex: regexPattern } 
+//   //// const pattern = /^(\d{4}-\d{2}-\d{2})/
+//   //__________________________________________
+
+//   Trip.find({departure: { $regex : new RegExp(departureInput, "i") }, arrival: { $regex : new RegExp(arrivalInput, "i")} }).then(dbData_FilteredByReq =>{
+    
+    
+//     for (let i =0; i< dbData_FilteredByReq.length; i++){
+//       //----------------> Converting ISO DB date into string
+//       const DbDate_str = dbData_FilteredByReq[i].date.toISOString()
+//       //----------------> Matching frontend date with DB date
+//       const searchResult = DbDate_str.startsWith(frontDateFormatted) 
+
+//       if (searchResult){
+//         const targetTrip = []
+//         targetTrip.push(dbData_FilteredByReq[i])
+//         console.log(targetTrip)
+//         //! ------------ RES not working !!!
+//         // res.json({ result: true, trips : targetTrip })
+//         // res.json({ result: true, trips : dbData_FilteredByReq[i] })
+//         //  res.json({ trips : targetTrip })
+//       }
+      
+//     }
+
+//   })
+  
+// })
+
+
+//!________________________________________________________________ Methode 2
+
+router.get('/', (req, res) => {
+  const { departure, arrival, date } = req.body;
+
+  //* ------------- EMPTY INPUT CHECKING -------------
+  if (!checkField(req.body, ["departure", "arrival", "date"])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
   }
-  // const departureInput = req.body.departure
-  // const arrivalInput = req.body.arrival
+  //* ---------- FrontEnd DATE FORMATING -----------------
+  const dateYear = date.substr(6, 9);
+  const dateMonth = date.substr(3, 2);
+  const dateDay = date.substr(0, 2);
+  const frontDateFormatted = `${dateYear}-${dateMonth}-${dateDay}`;
+  
+  //* ---------- Matching Trip Extraction -> DB -----------------
+  Trip.find({
+    departure: { $regex: new RegExp(departure, "i") },
+    arrival: { $regex: new RegExp(arrival, "i") }
+  }).then(dbData_FilteredByReq => {
+    const targetTrips = dbData_FilteredByReq.filter(trip => {
+      const dbDate_str = trip.date.toISOString();
+      return dbDate_str.startsWith(frontDateFormatted);
+    });
 
-  // Trip.find().then(data =>{
-  //   console.log(data)
-  //   res.json({ trips : data })
-  // })
-
-})
-
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
+    res.json({ result: true, trips: targetTrips });
+  }).catch(error => {
+    console.error(error);
+    res.json({ error: 'Sorry, could not find itinary' });
+  });
+});
 
 
 
